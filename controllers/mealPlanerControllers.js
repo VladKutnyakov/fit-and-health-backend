eb3681d013fd057c5396d5dc642d473c666662ab
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-04-28 10:37:51 +0400

    update meal planer day info controller

--- controllers/mealPlanerControllers.js
+++ controllers/mealPlanerControllers.js
@@ -3,25 +3,18 @@ const sequelize = require('../utils/dbConnect')
 const Op = Sequelize.Op;
 const MealPlanerInfo = require('../models/MealPlanerInfo')
 
+// Формат даты для всего проекта --- 2021-12-29
+
 module.exports.getMealPlanerInfo = async function (req, res) {
-  // Формат даты --- 2021.12.30
-
-  // convert date to unix timestamp
-  // new Date('2012.08.10').getTime() / 1000 // без значения времени
-
-  // получить текущую дату без времени
-  // console.log(Math.round(new Date().getTime() / 1000).toString());
-
   try {
     const mealPlanDayInfo = await sequelize.transaction( async (t) => {
-
       if (req.query && req.query.date) {
         // Поиск плана питания на день в соответствие с указанной датой
         const mealPlan = await MealPlanerInfo.findOne({
           where: {
             [Op.and]: [
               { userId: req.body.userId },
-              { date: new Date(req.query.date).toLocaleDateString() }
+              { date: new Date(req.query.date).getTime() / 1000 } // переданная дата (строка) приведенная в милесекунды
             ]
           },
         }, { transaction: t })
@@ -30,30 +23,16 @@ module.exports.getMealPlanerInfo = async function (req, res) {
           // Вернуть данные о найденном плане питания на указанный день
           return mealPlan
         } else {
-          // Создать пустую запись для указанной даты
-          const newMealPlan = await MealPlanerInfo.create({
-            userId: req.body.userId,
-            date: new Date(req.query.date).toLocaleDateString(),
-            targetProtein: null,
-            targetFats: null,
-            targetCarb: null,
-            targetWeight: null,
-            title: '',
-            description: '',
-            marks: null,
-            social: null,
-            mealParts: null
-          }, { transaction: t })
-
-          return newMealPlan
+          return false
         }
       } else {
+        const currentDate = new Date().toJSON().split('T')[0]
         // Поиск текущей даты, если дата не указана
         const mealPlan = await MealPlanerInfo.findOne({
           where: {
             [Op.and]: [
               { userId: req.body.userId },
-              { date: new Date().toLocaleDateString() }
+              { date: new Date(currentDate).getTime() / 1000 } // текущая дата в милисекундах
             ]
           },
         }, { transaction: t })
@@ -62,22 +41,8 @@ module.exports.getMealPlanerInfo = async function (req, res) {
           // Вернуть данные о найденном плане питания на текущий день
           return mealPlan
         } else {
-          // Создать и вернуть новую запись на текущий день
-          const newMealPlan = await MealPlanerInfo.create({
-            userId: req.body.userId,
-            date: new Date().toLocaleDateString(),
-            targetProtein: null,
-            targetFats: null,
-            targetCarb: null,
-            targetWeight: null,
-            title: '',
-            description: '',
-            marks: null,
-            social: null,
-            mealParts: null
-          }, { transaction: t })
   
-          return newMealPlan
+          return false
         }
       }
     })
@@ -94,4 +59,3 @@ module.exports.getMealPlanerInfo = async function (req, res) {
     res.status(500).json(error)
   }
 }
-
