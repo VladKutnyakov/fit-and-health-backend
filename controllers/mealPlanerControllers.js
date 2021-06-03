1d60d0abe51254df84af13ea84fb9d2662a5a16b
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-06-03 13:33:41 +0400

    update save meal planer info api

--- controllers/mealPlanerControllers.js
+++ controllers/mealPlanerControllers.js
@@ -41,7 +41,6 @@ module.exports.getMealPlanerInfo = async function (req, res) {
           // Вернуть данные о найденном плане питания на текущий день
           return mealPlan
         } else {
-  
           return false
         }
       }
@@ -64,44 +63,68 @@ module.exports.saveMealPlanerInfo = async function (req, res) {
   try {
     // console.log(req.body)
 
-    const mealPlan = await MealPlanerInfo.findOne({
-      where: {
-        [Op.and]: [
-          { userId: req.body.userId },
-          { id: req.body.mealPlanerInfo.id }
-        ]
-      },
-    })
-
-    if (mealPlan) {
-      // Вносим изменения в существующий план рациона на сутки
-      console.log('Запись уже есть в БД')
-    } else {
-      const currentDate = new Date().toJSON().split('T')[0]
-
-      const savedMealPlan = await MealPlanerInfo.create({
-        userId: req.body.userId,
-        date: new Date(currentDate).getTime() / 1000, // текущая дата в милисекундах
-        targetProtein: req.body.mealPlanerInfo.targetProtein,
-        targetFats: req.body.mealPlanerInfo.targetFats,
-        targetCarb: req.body.mealPlanerInfo.targetCarb,
-        targetWeight: req.body.mealPlanerInfo.targetWeight,
-        title: req.body.mealPlanerInfo.title,
-        description: req.body.mealPlanerInfo.description,
-        marks: JSON.stringify(req.body.mealPlanerInfo.marks),
-        social: JSON.stringify(req.body.mealPlanerInfo.social),
-        mealParts: JSON.stringify(req.body.mealPlanerInfo.mealParts)
+    // Если переданы параметры mealPlanerInfo и id выполнить поиск по БД
+    if (req.body.mealPlanerInfo && req.body.mealPlanerInfo.id) {
+      const candidate = await MealPlanerInfo.findOne({
+        where: {
+          [Op.and]: [
+            { userId: req.body.userId },
+            { id: req.body.mealPlanerInfo.id }
+          ]
+        },
       })
 
-      console.log(savedMealPlan.dataValues)
+      if (candidate) {
+        // Если запись в БД найдена, вносим изменения в существующий план рациона на сутки
+        console.log('Запись уже есть в БД')
 
-      const response = {
-        updatedToken: req.body.updatedToken,
-        data: {
-          mealPlanerInfo: savedMealPlan
+        const update = {
+          userId: req.body.userId,
+          date: new Date(currentDate).getTime() / 1000, // текущая дата в милисекундах
+          targetProtein: req.body.mealPlanerInfo.targetProtein,
+          targetFats: req.body.mealPlanerInfo.targetFats,
+          targetCarb: req.body.mealPlanerInfo.targetCarb,
+          targetWeight: req.body.mealPlanerInfo.targetWeight,
+          title: req.body.mealPlanerInfo.title,
+          description: req.body.mealPlanerInfo.description,
+          marks: JSON.stringify(req.body.mealPlanerInfo.marks),
+          social: JSON.stringify(req.body.mealPlanerInfo.social),
+          mealParts: JSON.stringify(req.body.mealPlanerInfo.mealParts)
         }
+        const params = {
+          where: {
+            id: candidate.id
+          }
+        }
+        await MealPlanerInfo.update(update, params)
+      } else {
+        // Если запись в БД не найдена, создаем план рациона на сутки
+        const currentDate = new Date().toJSON().split('T')[0]
+  
+        const savedMealPlan = await MealPlanerInfo.create({
+          userId: req.body.userId,
+          date: new Date(currentDate).getTime() / 1000, // текущая дата в милисекундах
+          targetProtein: req.body.mealPlanerInfo.targetProtein,
+          targetFats: req.body.mealPlanerInfo.targetFats,
+          targetCarb: req.body.mealPlanerInfo.targetCarb,
+          targetWeight: req.body.mealPlanerInfo.targetWeight,
+          title: req.body.mealPlanerInfo.title,
+          description: req.body.mealPlanerInfo.description,
+          marks: JSON.stringify(req.body.mealPlanerInfo.marks),
+          social: JSON.stringify(req.body.mealPlanerInfo.social),
+          mealParts: JSON.stringify(req.body.mealPlanerInfo.mealParts)
+        })
+  
+        console.log(savedMealPlan.dataValues)
+  
+        const response = {
+          updatedToken: req.body.updatedToken,
+          data: {
+            mealPlanerInfo: savedMealPlan
+          }
+        }
+        res.status(200).json(response)
       }
-      res.status(200).json(response)
     }
   } catch (error) {
     console.log(error)
