fccd853c737bc5dab2835c53c3fe31c0b0585a20
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-07-09 16:25:29 +0400

    update userParams model

--- models/UserParams.js
+++ models/UserParams.js
@@ -7,10 +7,82 @@ const UserParams = sequelize.define('user_params', {
     type: DataTypes.INTEGER,
     allowNull: true
   },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  middleName: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  gender: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  birthday: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
   weight: {
     type: DataTypes.FLOAT,
     allowNull: true
-  }
+  },
+  height: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  site: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  vk: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  facebook: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  instagram: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  youtube: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  twitter: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  skype: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  subscriptions: {
+    type: DataTypes.TEXT('large'),
+    allowNull: true
+  },
+  subscribers: {
+    type: DataTypes.TEXT('large'),
+    allowNull: true
+  },
+  sportDirections: {
+    type: DataTypes.TEXT('large'),
+    allowNull: true
+  },
 })
 
 module.exports = UserParams
--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require('sequelize')
 const sequelize = require('../utils/dbConnect')
 const Tokens = require('./Tokens')
+const UserParams = require('./UserParams')
 
 // Описание модели таблицы в БД MySQL
 const Users = sequelize.define('users', {
@@ -8,10 +9,10 @@ const Users = sequelize.define('users', {
     type: DataTypes.STRING,
     allowNull: true
   },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
+  // phone: {
+  //   type: DataTypes.STRING,
+  //   allowNull: true
+  // },
   password: {
     type: DataTypes.STRING,
     allowNull: true
@@ -19,6 +20,10 @@ const Users = sequelize.define('users', {
 })
 
 Users.hasMany(Tokens, {
+  foreignKey: 'userId'
+})
+
+Users.hasOne(UserParams, {
   foreignKey: 'userId'
 })
 
