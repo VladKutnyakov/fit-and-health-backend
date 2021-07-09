ffcd2a4b46036f93bb715e762d8ff00d9c32f5b0
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-07-09 13:53:22 +0400

    update delete route and add update product

--- routes/foodCalorieTableRoutes.js
+++ routes/foodCalorieTableRoutes.js
@@ -6,7 +6,8 @@ const router = express.Router()
 // http://localhost:3031/api/food-calorie-table/
 router.get('/', JwtGuard, foodCalorieTableControllers.getAllProducts)
 router.post('/save-product', JwtGuard, foodCalorieTableControllers.saveNewProduct)
-router.post('/remove-product', JwtGuard, foodCalorieTableControllers.removeProduct)
+router.put('/update-product', JwtGuard, foodCalorieTableControllers.updateProduct)
+router.delete('/remove-product/:id', JwtGuard, foodCalorieTableControllers.removeProduct)
 router.post('/change-favorite-param', JwtGuard, foodCalorieTableControllers.changeFavoriteParam)
 router.post('/change-pinned-param', JwtGuard, foodCalorieTableControllers.changePinnedParam)
 
