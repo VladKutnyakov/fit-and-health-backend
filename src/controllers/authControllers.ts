31e805614d3b71c5a61aa7306446debfba3c31bf
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-09-07 15:49:38 +0400

    update auth controller

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,92 +1,74 @@
 import { Request, Response } from "express"
-import { getManager } from "typeorm"
+import { getManager, Transaction, TransactionManager } from "typeorm"
 import { Users } from "../db/entities/Users"
-// import bcrypt from 'bcryptjs'
+import bcrypt from 'bcrypt'
 // import jwt from 'jsonwebtoken'
 
 // http://localhost:3031/api/auth/register/
 const register = async (req: Request, res: Response): Promise<Response> => {
+
   try {
     const entityManager = getManager()
 
     const candidate = await entityManager.findOne(Users, {where: {email: req.body.email}})
 
     if (candidate) {
-      console.log('Пользователь найден')
-    } else {
-      console.log('Пользователь не найден')
-    }
+      // console.log('Пользователь найден')
 
-    return res.status(200).json({
-      message: 'Пользователь зарегестрирован.'
-    })
+      return res.status(409).json({
+        message: 'Введенный E-mail уже используется. Попробуйте авторизироваться или используйте другой адрес электронной почты.'
+      })
+    } else {
+      // console.log('Пользователь не найден')
+
+      // Создаем нового пользователя в БД
+      // const salt = bcrypt.genSaltSync(10)
+      // const password = req.body.password
+
+      // @Transaction()
+      // save(@TransactionManager() manager: EntityManager, user: Users) {
+      //   let stud = new Student();
+      //   stud.name = student.name;
+      //   stud.entered = normalizeNumber(student.entered, 'Bad year entered');
+      //   stud.grade = normalizeNumber(student.grade, 'Bad grade');
+      //   stud.gender = student.gender;
+      //   await this.save(stud);
+      //   return stud.id;
+      // }
+
+      // const newUser = await Users.create({
+      //   email: req.body.email,
+      //   phone: req.body.phone,
+      //   password: bcrypt.hashSync(password, salt)
+      // })
+
+      // // Генерируем рефреш токен для нового пользователя
+      // const refreshToken = jwt.sign({
+      //   id: newUser.id,
+      // }, keys.jwtRefresh, {expiresIn: '30d'})
+
+      // // Генерируем token для нового пользователя
+      // const accessToken = jwt.sign({
+      //   id: newUser.id,
+      //   refreshToken: refreshToken
+      // }, keys.jwt, {expiresIn: '15m'})
+
+      // // Создаем запись с токенами доступа для нового пользователя
+      // await Tokens.create({
+      //   userId: candidate.id,
+      //   accessToken,
+      //   refreshToken
+      // })
+
+      // return res.status(200).json(accessToken)
+      return res.status(200).json({mess: '123'})
+    }
   } catch (error: any) {
     return res.status(500).json({
       message: 'Неизвестная ошибка.'
     })
   }
 
-  // try {
-  //   const candidate = await Users.findOne({
-  //     where: {
-  //       email: req.body.email
-  //       // [Op.or]: [
-  //       //   { email: req.body.email },
-  //       //   { phone: req.body.phone }
-  //       // ]
-  //     }
-  //   })
-
-  //   if (!candidate) {
-  //     // создать пользователя в БД
-  //     try {
-  //       // Создаем нового пользователя в БД
-  //       const salt = bcrypt.genSaltSync(10)
-  //       const password = req.body.password
-  //       const newUser = await Users.create({
-  //         email: req.body.email,
-  //         phone: req.body.phone,
-  //         password: bcrypt.hashSync(password, salt)
-  //       })
-
-  //       // Генерируем рефреш токен для нового пользователя
-  //       const refreshToken = jwt.sign({
-  //         id: newUser.id,
-  //       }, keys.jwtRefresh, {expiresIn: '30d'})
-
-  //       // Генерируем token для нового пользователя
-  //       const accessToken = jwt.sign({
-  //         id: newUser.id,
-  //         refreshToken: refreshToken
-  //       }, keys.jwt, {expiresIn: '15m'})
-
-  //       // Создаем запись с токенами доступа для нового пользователя
-  //       await Tokens.create({
-  //         userId: candidate.id,
-  //         accessToken,
-  //         refreshToken
-  //       })
-
-  //       res.status(200).json(accessToken)
-  //     } catch (error) {
-  //       res.status(404).json({
-  //         error,
-  //         message: 'Внутренняя ошибка сервера. Попробуйте еще раз.'
-  //       })
-  //     }
-  //   } else {
-  //     // если пользователь существеует то возвращаем ошибку
-  //     res.status(409).json({
-  //       message: 'Введенный E-mail уже занят. Попробуйте авторизироваться или используйте другой E-mail.'
-  //     })
-  //   }
-  // } catch (error) {
-  //   console.log(error)
-
-  //   res.status(400).json({
-  //     message: 'Неверный запрос.'
-  //   })
-  // }
 }
 
 // http://localhost:3031/api/auth/login/
