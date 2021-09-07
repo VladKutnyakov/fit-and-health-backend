6fbfdd74b9480b60439f29a96a5d43ce3c746673
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-09-07 14:20:19 +0400

    update user entity model

--- src/db/entities/Users.ts
+++ src/db/entities/Users.ts
@@ -11,7 +11,7 @@ export class Users {
     type: 'text',
     nullable: false,
     unique: true,
-    comment: 'Электроннач почта пользователя'
+    comment: 'Электронная почта пользователя'
   })
   @IsEmail()
   email: string
@@ -23,5 +23,137 @@ export class Users {
     comment: 'Пароль пользователя для входа'
   })
   password: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Имя пользователя'
+  })
+  firstName: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Отчество пользователя'
+  })
+  middleName: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Фамилия пользователя'
+  })
+  lastName: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Дата рождения пользователя'
+  })
+  birthday: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Дата рождения пользователя'
+  })
+  phone: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Пол пользователя'
+  })
+  gender: string
+
+  @Column({
+    type: 'integer',
+    nullable: false,
+    unique: false,
+    comment: 'Вес пользователя'
+  })
+  weight: number
+
+  @Column({
+    type: 'integer',
+    nullable: false,
+    unique: false,
+    comment: 'Рост пользователя'
+  })
+  height: number
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Город проживания пользователя'
+  })
+  city: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Личный сайт пользователя'
+  })
+  site: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Ссылка на аккаунт в vk пользователя'
+  })
+  vk: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Ссылка на аккаунт в facebook пользователя'
+  })
+  facebook: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Ссылка на аккаунт в instagram пользователя'
+  })
+  instagram: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Ссылка на аккаунт в youtube пользователя'
+  })
+  youtube: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Ссылка на аккаунт в twitter пользователя'
+  })
+  twitter: string
+
+  @Column({
+    type: 'text',
+    nullable: false,
+    unique: false,
+    comment: 'Ссылка на аккаунт в skype пользователя'
+  })
+  skype: string
+
+  // subscriptions
+  // subscribers
+  // sportDirections
 
 }
