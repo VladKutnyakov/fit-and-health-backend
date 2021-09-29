bedcdc725c26c24c02c259b9eed8bc349499528d
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-09-29 13:12:34 +0400

    update swagger docs

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -37,6 +37,49 @@ const router: Router = express.Router()
 */
 router.post('/login', authControllers.login)
 
+/**
+* @swagger
+* /api/auth/register:
+*   post:
+*     tags:
+*       - Авторизация
+*     summary: Регистрация пользователя
+*     requestBody:
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             required: true
+*             properties:
+*               email:
+*                 type: string
+*                 description: Email пользователя
+*               password:
+*                 type: string
+*                 description: Пароль пользователя
+*     responses:
+*       200:
+*         description: Token авторизации
+*         content:
+*           text/plain:
+*             schema:
+*               type: string
+*               example: token_string
+*       409:
+*         description: Конфликт введенных данных
+*         content:
+*           application/json:
+*             schema:
+*               type: object
+*               required: true
+*               properties:
+*                 message:
+*                   type: string
+*                   description: Текст ошибки
+*               example: Введенный E-mail уже используется.
+*       500:
+*         description: Неизвестная ошибка. Обратитесь к разработчику.
+*/
 router.post('/register', authControllers.register)
 
 export default router
