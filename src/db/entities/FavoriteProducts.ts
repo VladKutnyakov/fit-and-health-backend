b125faa13f71815941ffe26b6b468869e3ee4658
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-11-01 13:40:16 +0400

    add model

---
+++ src/db/entities/FavoriteProducts.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryColumn, ManyToMany } from "typeorm"
+import { Products } from './Products'
+import { Users } from "./Users"
+
+@Entity()
+export class FavoriteProducts {
+
+  @PrimaryColumn({
+    nullable: false,
+    comment: 'ID пользователя'
+  })
+  @ManyToMany(() => Users)
+  userId: number
+
+  @PrimaryColumn({
+    nullable: false,
+    comment: 'ID продукта'
+  })
+  @ManyToMany(() => Products)
+  productId: number
+
+}
