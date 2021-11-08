47f7aae77013277e5840a14b2f4203341f4a29f6
Korobov Dmitry <RedFrag@yandex.ru>
2021-11-08 21:12:46 +0400

    remove dont used files

--- src/routes/trainingDiaryRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from 'express'
-import trainingDiaryControllers from '../controllers/trainingDiaryControllers'
-import JwtGuard from '../middleware/Guards/JwtGuard'
-const router: Router = express.Router()
-
-// http://localhost:3000/api/recipe/:id
-router.get('/', JwtGuard, trainingDiaryControllers.fetchMainTrainingProgram)
-
-export default router
