f77fd38338317fa74b27639a79350a68371e06a0
Korobov Dmitry <RedFrag@yandex.ru>
2021-11-08 21:02:54 +0400

    update exercises controllers

---
+++ src/controllers/ExercisesControllers.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express"
+import { getRepository, getConnection } from "typeorm"
+// import { Products } from "../db/entities/Products"
+// import { ProductCategories } from '../db/entities/ProductCategories'
+// import { Users } from '../db/entities/Users'
+
+const getProductCategories = async (req: Request, res: Response): Promise<Response> => {
+  try {
+
+    const response = {
+      updatedToken: req.body.updatedToken,
+      data: null
+    }
+
+    return res.status(200).json(response)
+  } catch (error: any) {
+    return res.status(500).json({
+      updatedToken: req.body.updatedToken,
+      errorMessage: 'Неизвестная ошибка.'
+    })
+  }
+}
+
+const fetchExerciseInfo = async (req: Request, res: Response): Promise<Response> => {
+  try {
+
+    const response = {
+      updatedToken: req.body.updatedToken,
+      data: null
+    }
+
+    return res.status(200).json(response)
+  } catch (error: any) {
+    return res.status(500).json({
+      updatedToken: req.body.updatedToken,
+      errorMessage: 'Неизвестная ошибка.'
+    })
+  }
+}
+
+const saveNewExercise = async (req: Request, res: Response): Promise<Response> => {
+  try {
+
+    const response = {
+      updatedToken: req.body.updatedToken,
+      data: null
+    }
+
+    return res.status(200).json(response)
+  } catch (error: any) {
+    return res.status(500).json({
+      updatedToken: req.body.updatedToken,
+      errorMessage: 'Неизвестная ошибка.'
+    })
+  }
+}
+
+export default {
+  getProductCategories,
+  fetchExerciseInfo,
+  saveNewExercise
+}
