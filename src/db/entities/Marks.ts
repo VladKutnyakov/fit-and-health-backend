d2c4f83fd6885d620cdfb2613aeaf001ca56439e
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-11-16 10:18:08 +0400

    add model

---
+++ src/db/entities/Marks.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany } from 'typeorm'
+import { MealPlaners } from './MealPlaners'
+
+@Entity()
+export class Marks {
+
+  @PrimaryGeneratedColumn({
+    comment: 'ID отметки'
+  })
+  id: number
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    unique: false,
+    comment: 'Название отметки'
+  })
+  title: string
+
+  @ManyToMany(() => MealPlaners, exercise => exercise.marks)
+  mealPlanerMarks: MealPlaners[]
+
+}
