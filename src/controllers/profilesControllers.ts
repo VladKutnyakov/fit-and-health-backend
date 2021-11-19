8e622fad112adfb9c9cf3301fe0d519605eaf612
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-11-19 14:08:33 +0400

    update getProfilesList controller

--- src/controllers/profilesControllers.ts
+++ src/controllers/profilesControllers.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from "express"
-import { getManager } from "typeorm"
+import { getManager, getRepository } from "typeorm"
 import { UsersProfiles } from "../db/entities/UsersProfiles"
 
 const getProfilesList = async (req: Request, res: Response): Promise<Response> => {
   try {
+    const ProfilesList = await getRepository(UsersProfiles)
+      .createQueryBuilder('usersProfiles')
+      .leftJoin("usersProfiles.user", "user")
+      .addSelect(['user.id'])
+      .orderBy({'usersProfiles.id': 'ASC'})
+      .getMany()
+    console.log(ProfilesList)
 
     const response = {
       updatedToken: req.body.updatedToken,
-      data: null
+      data: ProfilesList
     }
 
     return res.status(200).json(response)
