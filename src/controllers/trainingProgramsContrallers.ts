2cd4c5f7353e1ce0779e9a9854e9d22172cf40dc
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-11-19 16:33:36 +0400

    update getTrainingProgramInfo controller

--- src/controllers/trainingProgramsContrallers.ts
+++ src/controllers/trainingProgramsContrallers.ts
@@ -39,11 +39,13 @@ const getTrainingProgramInfo = async (req: Request, res: Response): Promise<Resp
       .createQueryBuilder('trainingProgram')
       .where([{id: req.params.trainingProgramId}])
       .leftJoinAndSelect("trainingProgram.trainingProgramDays", "trainingProgramDays")
+      .leftJoinAndSelect('trainingProgramDays.trainingProgramDayExercises', 'trainingProgramDayExercises')
+      .leftJoinAndSelect('trainingProgramDayExercises.exercise', 'exercise')
       .leftJoinAndSelect('trainingProgram.marks', 'marks')
       .leftJoin("trainingProgram.user", "user")
       .addSelect(['user.id'])
       .getOne()
-    console.log(TrainingProgramInfo)
+    console.log(TrainingProgramInfo?.trainingProgramDays[0])
 
     const response = {
       updatedToken: req.body.updatedToken,
