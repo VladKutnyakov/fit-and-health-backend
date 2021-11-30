0591a087f076004167ae63da490cf5d650e08465
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2021-11-30 10:20:42 +0400

    add image field

--- src/db/entities/Muscles.ts
+++ src/db/entities/Muscles.ts
@@ -17,6 +17,14 @@ export class Muscles {
   })
   title: string
 
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    unique: false,
+    comment: 'Изображение мышечной группы'
+  })
+  previewImage: string
+
   @OneToMany(() => Exercises, exercises => exercises.muscleGroup)
   exercises: Exercises[]
 
