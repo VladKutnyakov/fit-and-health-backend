76e4516b2ec50f4e6131a10dd5900ecaa36398fc
Korobov Dmitry <redfrag@yandex.ru>
2022-03-18 09:24:55 +0400

    update getTrainingDayInfo controller

--- src/controllers/trainingProcess/controllers/getTrainingDayInfo.ts
+++ src/controllers/trainingProcess/controllers/getTrainingDayInfo.ts
@@ -3,12 +3,12 @@ import { getRepository } from "typeorm"
 import { TrainingProgramDays } from '../../../db/entities/TrainingProgramDays'
 
 export const getTrainingDayInfo = async (req: Request, res: Response): Promise<Response> => {
-  // console.log(req.query.trainingDayId)
+  // console.log(req.query.trainingDay)
 
   try {
     const TrainingDayInfo = await getRepository(TrainingProgramDays)
       .createQueryBuilder('trainingProgramDays')
-      .where([{id: req.query.trainingDayId}])
+      .where([{id: req.query.trainingDay}])
       .leftJoin("trainingProgramDays.trainingProgramDayExercises", "trainingProgramDayExercises")
       .addSelect([
         'trainingProgramDayExercises.approaches',
@@ -23,11 +23,13 @@ export const getTrainingDayInfo = async (req: Request, res: Response): Promise<R
       .getOne()
     // console.log(TrainingDayInfo?.trainingProgramDayExercises[0].exercise.exerciseApproaches)
 
-    const response = {
-      data: TrainingDayInfo
+    if (TrainingDayInfo) {
+      return res.status(200).json(TrainingDayInfo)
+    } else {
+      return res.status(400).json({
+        errorMessage: 'Тренировочный день не найден.'
+      })
     }
-
-    return res.status(200).json(response)
   } catch (error: any) {
     return res.status(500).json({
       errorMessage: 'Неизвестная ошибка.'
