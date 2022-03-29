932479c7868f3b1ac06d78f58d3f9b730fbb7e40
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2022-03-29 10:18:54 +0400

    update typeorm

--- src/controllers/foodCalorieTable/controllers/getAllProducts.ts
+++ src/controllers/foodCalorieTable/controllers/getAllProducts.ts
@@ -6,7 +6,6 @@ export const getAllProducts = async (req: Request, res: Response): Promise<Respo
   try {
     const ProductsList = await dataSource.getRepository(Products)
       .createQueryBuilder('products')
-      // .where([{user: req.body.userId}, {user: null}])
       .where("products.user = :id OR products.user IS NULL", { id: req.body.userId })
       .leftJoin("products.user", "user")
       .addSelect(['user.id'])
