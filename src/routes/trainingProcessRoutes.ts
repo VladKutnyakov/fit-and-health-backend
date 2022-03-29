370f2ef75493630c2b79f70168cf92b9681c5d3c
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2022-03-29 13:32:34 +0400

    update swagger

--- src/routes/trainingProcessRoutes.ts
+++ src/routes/trainingProcessRoutes.ts
@@ -105,15 +105,50 @@ router.get('/training-program-info', JwtGuard, trainingProcessContrallers.getTra
 *             schema:
 *               type: object
 *               properties:
-*                 updatedToken:
-*                   type: string
-*                   description: Обновленный токен авторизации
-*                 data:
+*                 trainingDay:
 *                   type: object
 *                   properties:
 *                     id:
 *                       type: integer
-*                       description: Id тренировочной программы
+*                       description: ID дня тренировочной программы
+*                     title:
+*                       type: string
+*                       description: Название дня тренировочной программы
+*                 trainingType:
+*                   type: object
+*                   properties:
+*                     id:
+*                       type: integer
+*                       description: ID типа дня тренировочной программы
+*                     title:
+*                       type: string
+*                       description: Название типа дня тренировочной программы
+*                 comment:
+*                   type: string
+*                   description: Комментарий к тренировочному дню
+*                 trainingProgramDayExercises:
+*                   type: array
+*                   items:
+*                     type: object
+*                     properties:
+*                       isActive:
+*                         type: boolean
+*                         description: Признак активно выполнение упражнения или нет
+*                       title:
+*                         type: string
+*                         description: Название подхода
+*                       repeats:
+*                         type: integer
+*                         description: Кол-во повторений в подходе
+*                       additionalWeight:
+*                         type: integer
+*                         description: Дополнительный вес при выполнении подхода
+*                       implementationTime:
+*                         type: integer
+*                         description: Время выполнения
+*                       restTime:
+*                         type: integer
+*                         description: Время восстановления
 *       401:
 *         description: Ошибка авторизации
 */
