feffa26921eac8775fd02acc8b73f9e52e98a36b
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2022-05-25 15:26:59 +0400

    update

--- src/middleware/Guards/JwtGuard.ts
+++ src/middleware/Guards/JwtGuard.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 import { dataSource } from '../../dataSource'
+// import { Users } from "../../db/entities/Users"
 import { Tokens } from "../../db/entities/Tokens"
 interface I_JwtKeys {
   jwt: any
@@ -13,38 +14,69 @@ const keys: I_JwtKeys = {
 }
 
 export default async function JwtGuard (req: Request, res: Response, next: NextFunction) {
-  // найти все токены
-  // удалить те у которых истек срок
-  // Если токен текущий остался все ок, если нет - ошибка авторизации
-
-  // const DeletedToken = await dataSource
-  //   .createQueryBuilder()
-  //   .delete()
-  //   .from(Tokens)
-  //   .where(`accessToken = '${AccessToken}'`)
-  //   .execute()
-  // // console.log(DeletedToken)
-
-
   if (req.headers && req.headers.authorization) {
     const AccessToken: string = req.headers.authorization.replace('Bearer ', '')
     const DecodedToken: any = jwt.decode(AccessToken, keys.jwt)
+    let accessTokenIsFound = false // признак есть токен в БД или нет
 
-    jwt.verify(AccessToken, keys.jwt, async (error: any) => {
-      if (error) {
-        return res.status(401).json({
-          errors: [
-            {
-              field: null,
-              errorMessage: 'Ошибка авторизации.'
-            }
-          ]
-        })
-      } else {
-        req.body.userId = DecodedToken.id
-        next()
+    // Удаление не валидных токенов из БД для пользователя --- НАЧАЛО
+    const UserTokens = await dataSource.getRepository(Tokens)
+      .createQueryBuilder('tokens')
+      .select([
+        'tokens.accessToken'
+      ])
+      .where(`tokens.userId = '${DecodedToken.id}'`)
+      .leftJoin('tokens.user', 'user')
+      .addSelect(['user.id'])
+      .getMany()
+    // console.log(UserTokens)
+
+    for (let i = 0; i < UserTokens.length; i++) {
+      if (UserTokens[i].accessToken === AccessToken) {
+        accessTokenIsFound = true
       }
-    })
+
+      jwt.verify(UserTokens[i].accessToken, keys.jwt, async (error: any) => {
+        if (error) {
+          await dataSource
+            .createQueryBuilder()
+            .delete()
+            .from(Tokens)
+            .where(`accessToken = '${UserTokens[i].accessToken}'`)
+            .execute()
+        }
+      })
+    }
+    // Удаление не валидных токенов из БД для пользователя --- КОНЕЦ
+
+    // Если accessToken был наден в БД проверяем его валидность
+    if (accessTokenIsFound) {
+      jwt.verify(AccessToken, keys.jwt, async (error: any) => {
+        if (error) {
+          return res.status(401).json({
+            errors: [
+              {
+                field: null,
+                errorMessage: 'Ошибка авторизации.'
+              }
+            ]
+          })
+        } else {
+          req.body.userId = DecodedToken.id
+          next()
+        }
+      })
+    } else {
+      // Если accessToken НЕТ в БД
+      return res.status(401).json({
+        errors: [
+          {
+            field: null,
+            errorMessage: 'Ошибка авторизации.'
+          }
+        ]
+      })
+    }
   } else {
     req.body.userId = null
     next()
