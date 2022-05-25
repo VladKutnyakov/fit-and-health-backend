9849ea9d2f6046c9f449966aee6cad5abf883dfb
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2022-05-25 15:12:15 +0400

    update

--- src/middleware/Guards/JwtGuard.ts
+++ src/middleware/Guards/JwtGuard.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
-
+import { dataSource } from '../../dataSource'
+import { Tokens } from "../../db/entities/Tokens"
 interface I_JwtKeys {
   jwt: any
   // jwtRefresh: any
@@ -12,11 +13,24 @@ const keys: I_JwtKeys = {
 }
 
 export default async function JwtGuard (req: Request, res: Response, next: NextFunction) {
-  if (req.headers && req.headers.authorization) {
-    const token: string = req.headers.authorization.replace('Bearer ', '')
-    const decodedToken: any = jwt.decode(token, keys.jwt)
+  // найти все токены
+  // удалить те у которых истек срок
+  // Если токен текущий остался все ок, если нет - ошибка авторизации
 
-    jwt.verify(token, keys.jwt, async (error: any) => {
+  // const DeletedToken = await dataSource
+  //   .createQueryBuilder()
+  //   .delete()
+  //   .from(Tokens)
+  //   .where(`accessToken = '${AccessToken}'`)
+  //   .execute()
+  // // console.log(DeletedToken)
+
+
+  if (req.headers && req.headers.authorization) {
+    const AccessToken: string = req.headers.authorization.replace('Bearer ', '')
+    const DecodedToken: any = jwt.decode(AccessToken, keys.jwt)
+
+    jwt.verify(AccessToken, keys.jwt, async (error: any) => {
       if (error) {
         return res.status(401).json({
           errors: [
@@ -27,7 +41,7 @@ export default async function JwtGuard (req: Request, res: Response, next: NextF
           ]
         })
       } else {
-        req.body.userId = decodedToken.id
+        req.body.userId = DecodedToken.id
         next()
       }
     })
