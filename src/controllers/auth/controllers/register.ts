f315fc14912257d160d77da0db5c5c00003d4035
korobov-dv@serviceit.su <korobov-dv@serviceit.su>
2022-05-25 11:33:58 +0400

    update

--- src/controllers/auth/controllers/register.ts
+++ src/controllers/auth/controllers/register.ts
@@ -47,9 +47,7 @@ export const register = async (req: Request, res: Response): Promise<Response> =
     const User = await dataSource.getRepository(Users)
       .createQueryBuilder('users')
       .select([
-        'users.id',
-        'users.email',
-        'users.password'
+        'users.id'
       ])
       .where(`users.email = '${req.body.email}'`)
       .getOne()
@@ -60,97 +58,64 @@ export const register = async (req: Request, res: Response): Promise<Response> =
         message: 'Введенный E-mail уже используется. Попробуйте авторизироваться или используйте другой адрес электронной почты.'
       })
     } else {
-      const password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+      // ТРАНЗАКЦИЯ
+      const queryRunner = dataSource.createQueryRunner()
+      await queryRunner.connect()
 
-      const CreatedUser = await dataSource.getRepository(Users)
-        .createQueryBuilder('users')
-        .insert()
-        .into(Users)
-        .values([
-          {
-            email: req.body.email,
-            phone: req.body.phone,
-            password: password,
-          }
-        ])
-        .execute()
-      // console.log(CreatedUser)
+      await queryRunner.startTransaction()
 
-      const JwtKey: Secret = process.env.JWT || 'access'
-      const AccessToken = jwt.sign({ id: CreatedUser.identifiers[0].id }, JwtKey, { expiresIn: '30d' })
+      try {
+        const password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
 
-      const CreatedAccessToken = await dataSource.getRepository(Tokens)
-          .createQueryBuilder('tokens')
+        const CreatedUser = await dataSource.getRepository(Users)
+          .createQueryBuilder('users')
           .insert()
-          .into(Tokens)
+          .into(Users)
           .values([
             {
-              accessToken: AccessToken,
-              user: dataSource.getRepository(Users).create({
-                id: CreatedUser.identifiers[0].id,
-              })
+              email: req.body.email,
+              phone: req.body.phone,
+              password: password,
             }
           ])
           .execute()
-        // console.log(CreatedAccessToken)
+        // console.log(CreatedUser)
+
+        const JwtKey: Secret = process.env.JWT || 'access'
+        const AccessToken = jwt.sign({ id: CreatedUser.identifiers[0].id }, JwtKey, { expiresIn: '30d' })
+
+        const CreatedAccessToken = await dataSource.getRepository(Tokens)
+            .createQueryBuilder('tokens')
+            .insert()
+            .into(Tokens)
+            .values([
+              {
+                accessToken: AccessToken,
+                user: dataSource.getRepository(Users).create({
+                  id: CreatedUser.identifiers[0].id,
+                })
+              }
+            ])
+            .execute()
+          // console.log(CreatedAccessToken)
+
+        await queryRunner.commitTransaction()
 
         return res.status(200).json(CreatedAccessToken.identifiers[0].accessToken)
-    }
-
-
-
-
-
-
-
-
-    const candidate = await dataSource.manager.findOne(Users, {where: {email: req.body.email}})
-
-    if (candidate) {
-      // console.log('Пользователь найден')
-
-      return res.status(409).json({
-        message: 'Введенный E-mail уже используется. Попробуйте авторизироваться или используйте другой адрес электронной почты.'
-      })
-    } else {
-      // console.log('Пользователь не найден')
-
-      // Создаем нового пользователя в БД
-      const CreatedUserAccessToken = await dataSource.transaction(async transactionalEntityManager => {
-
-        // Создать пользователя
-        const NewUser = new Users()
-        NewUser.email = req.body.email
-        NewUser.phone = req.body.phone
-        NewUser.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-
-        const CreatedUser = await transactionalEntityManager.save(NewUser)
-
-        // Генерируем рефреш токен для созданного пользователя
-        const JwtRefreshKey: Secret = process.env.JWT_REFRESH || ''
-        const RefreshToken = jwt.sign({
-          id: CreatedUser.id,
-        }, JwtRefreshKey, {expiresIn: '30d'})
-
-        // Генерируем token для созданного пользователя
-        const JwtKey: Secret = process.env.JWT || ''
-        const AccessToken = jwt.sign({
-          id: CreatedUser.id,
-          refreshToken: RefreshToken
-        }, JwtKey, {expiresIn: '15m'})
-
-        // Создаем запись с токенами доступа для созданного пользователя
-        const CreatedTokens = new Tokens()
-        CreatedTokens.accessToken = AccessToken
-        // CreatedTokens.refreshToken = RefreshToken
-        CreatedTokens.user = CreatedUser
-
-        await transactionalEntityManager.save(CreatedTokens)
-
-        return AccessToken
-      })
-
-      return res.status(200).json(CreatedUserAccessToken)
+      } catch (error) {
+        await queryRunner.rollbackTransaction()
+  
+        return res.status(400).json({
+          errors: [
+            {
+              field: null,
+              errorMessage: 'Ошибка при регистрации.'
+            }
+          ]
+        })
+      } finally {
+        await queryRunner.release()
+      }
     }
   } catch (error: any) {
     return res.status(500).json({
@@ -161,3 +126,52 @@ export const register = async (req: Request, res: Response): Promise<Response> =
 }
 
 // СТАРЫЙ КОД с refreshToken
+
+// const candidate = await dataSource.manager.findOne(Users, {where: {email: req.body.email}})
+
+//   if (candidate) {
+//     // console.log('Пользователь найден')
+
+//     return res.status(409).json({
+//       message: 'Введенный E-mail уже используется. Попробуйте авторизироваться или используйте другой адрес электронной почты.'
+//     })
+//   } else {
+//     // console.log('Пользователь не найден')
+
+//     // Создаем нового пользователя в БД
+//     const CreatedUserAccessToken = await dataSource.transaction(async transactionalEntityManager => {
+
+//       // Создать пользователя
+//       const NewUser = new Users()
+//       NewUser.email = req.body.email
+//       NewUser.phone = req.body.phone
+//       NewUser.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+
+//       const CreatedUser = await transactionalEntityManager.save(NewUser)
+
+//       // Генерируем рефреш токен для созданного пользователя
+//       const JwtRefreshKey: Secret = process.env.JWT_REFRESH || ''
+//       const RefreshToken = jwt.sign({
+//         id: CreatedUser.id,
+//       }, JwtRefreshKey, {expiresIn: '30d'})
+
+//       // Генерируем token для созданного пользователя
+//       const JwtKey: Secret = process.env.JWT || ''
+//       const AccessToken = jwt.sign({
+//         id: CreatedUser.id,
+//         refreshToken: RefreshToken
+//       }, JwtKey, {expiresIn: '15m'})
+
+//       // Создаем запись с токенами доступа для созданного пользователя
+//       const CreatedTokens = new Tokens()
+//       CreatedTokens.accessToken = AccessToken
+//       // CreatedTokens.refreshToken = RefreshToken
+//       CreatedTokens.user = CreatedUser
+
+//       await transactionalEntityManager.save(CreatedTokens)
+
+//       return AccessToken
+//     })
+
+//     return res.status(200).json(CreatedUserAccessToken)
+//   }
